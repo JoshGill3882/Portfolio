d3a5a3dc344ae4f5b9ff7d60f2373c740bcf363a
JoshGill3882 <joshgill2013@gmail.com>
2025-01-22 23:39:40 +0000

    Homepage Header text altered

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,8 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title"> JoshGill.dev </Heading>
-        <p className="hero_subtitle"> Welcome to my Portfolio </p>
+        <Heading as="h1" className="hero__title">Hi, I'm Josh</Heading>
+        <p className="hero_subtitle">Welcome to my Portfolio</p>
       </div>
     </header>
   );
