b7807dddc41e26c3c3d3083cb040cb0adde1b27f
JoshGill3882 <joshgill2013@gmail.com>
2025-05-28 14:57:00 +0100

    "HomepageHeader" Component extracted to separate component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,9 @@
 import type { ReactNode } from 'react';
-import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
 
-import styles from './index.module.css';
+import { HomepageHeader } from '../components/homepageHeader';
 import { Specialties } from "../components/specialties";
-
-function HomepageHeader() {
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">Hi, I'm Josh</Heading>
-        <p className="hero_subtitle">Welcome to my Portfolio</p>
-      </div>
-    </header>
-  );
-}
 
 export default function Home(): ReactNode {
   return (
