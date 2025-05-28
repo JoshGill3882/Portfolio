5ad749d0bd06565a30249deff495768995487eb1
JoshGill3882 <joshgill2013@gmail.com>
2025-05-28 16:44:32 +0100

    Files renamed to fit more to their contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import type { ReactNode } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
-import { HomepageHeader } from '../components/homepageHeader';
-import { Specialties } from "../components/specialties";
+import { HomepageHeader } from '../components/homepageHeader/homepageHeader';
+import { Specialties } from "../components/specialties/specialties";
 
 export default function Home(): ReactNode {
   return (
